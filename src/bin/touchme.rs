fead1b4505306e67df6efb6fe6e32999ae296edf
Sami J. Mäkinen <sjm@iki.fi>
2026-02-03 17:34:34 +0200

    tweaks

--- src/bin/touchme.rs
+++ src/bin/touchme.rs
@@ -3,18 +3,21 @@
 use touchme::*;
 
 const RE_YYYYMMDD: &str = r"(\d\d\d\d)(\d\d)(\d\d)";
+const RE_YYYY_MM_DD: &str = r"(\d\d\d\d).(\d\d).(\d\d)";
 
 fn main() -> anyhow::Result<()> {
     let me = env!("CARGO_BIN_NAME");
     let opts = OptsCommon::parse();
     opts.start_pgm(me);
-    let re_date = Regex::new(RE_YYYYMMDD)?;
+    let re1 = Regex::new(RE_YYYYMMDD)?;
+    let re2 = Regex::new(RE_YYYY_MM_DD)?;
+
     debug!("opts: {opts:?}");
 
     for a in &opts.args {
         let md = fs::metadata(a)?;
         if md.is_file() {
-            if let Some(ts) = detect_date_filename(path::Path::new(a), &re_date)
+            if let Some(ts) = detect_date_filename(path::Path::new(a), &[&re1, &re2])
                 && !opts.dry_run
             {
                 update_timestamp(path::Path::new(a), ts)?;
@@ -28,7 +31,7 @@ fn main() -> anyhow::Result<()> {
             let files = recurse_subdir(path::Path::new(a))?;
             debug!("Found {} files:\n{:?}", files.len(), files);
             for f in files {
-                if let Some(ts) = detect_date_filename(&f, &re_date)
+                if let Some(ts) = detect_date_filename(&f, &[&re1, &re2])
                     && !opts.dry_run
                 {
                     update_timestamp(&f, ts)?;
@@ -60,24 +63,25 @@ fn recurse_subdir(p: &path::Path) -> anyhow::Result<Vec<path::PathBuf>> {
     Ok(files)
 }
 
-fn detect_date_filename(file_path: &path::Path, re_date: &Regex) -> Option<time::SystemTime> {
+fn detect_date_filename(file_path: &path::Path, re_date: &[&Regex]) -> Option<time::SystemTime> {
     let filename = file_path.file_name()?.to_string_lossy();
     debug!("filename: {filename:?}");
 
-    if let Some(caps) = re_date.captures(&filename) {
-        let (year, month, day) = (
-            caps[1].parse::<i32>().ok()?,
-            caps[2].parse::<u32>().ok()?,
-            caps[3].parse::<u32>().ok()?,
-        );
-        let ts = NaiveDate::from_ymd_opt(year, month, day)?
-            .and_hms_opt(0, 0, 0)?
-            .and_utc();
-        debug!("Detected {year:04}-{month:02}-{day:02}");
-        Some(ts.into())
-    } else {
-        None
+    for re in re_date {
+        if let Some(caps) = re.captures(&filename) {
+            let (year, month, day) = (
+                caps[1].parse::<i32>().ok()?,
+                caps[2].parse::<u32>().ok()?,
+                caps[3].parse::<u32>().ok()?,
+            );
+            let ts = NaiveDate::from_ymd_opt(year, month, day)?
+                .and_hms_opt(0, 0, 0)?
+                .and_utc();
+            debug!("Detected {year:04}-{month:02}-{day:02}");
+            return Some(ts.into());
+        }
     }
+    None
 }
 
 fn update_timestamp(file_path: &path::Path, ts: time::SystemTime) -> anyhow::Result<()> {
